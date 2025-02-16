fcd5606e2a9e7b7ee0216d6f202a24caa754f9a7
exp-ahn <wltn24996@gmail.com>
2025-02-16 19:55:36 +0900

    Data binding and init style code of react grammer

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,17 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
+
+  let post: string = '송파구 맛집 리스트';
+  // document.querySelector('h4').innerHTML = post;
+
   return (
-    <div>
+    <div className='App'>
+      <div className='black-nav'>
+        {/* 이런 식으로 id값에 변수를 바인딩을 걸어줄 수도 있음. */}
+        <h4 style = { {color : '#7367f0', fontSize : '16px'} }>Ahnsta_Blog</h4>
+      </div>
+      <h4 id={post}>{ post }</h4>
     </div>
   )
 }
